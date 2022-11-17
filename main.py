d956be564bad77ed61ced5252e5ab3ea49716395
RaiymbekKokteubai <rkokteubai@gmail.com>
2022-11-17 23:10:17 +0600

    debugging code

--- main.py
+++ main.py
@@ -1,22 +1,18 @@
 import streamlit as st
-from PIL import Image
 import requests
 import json
+import time
 
-API_TOKEN = "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"
+API_TOKEN = "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"
 API_URL = "https://api-inference.huggingface.co/models/gianlab/swin-tiny-patch4-window7-224-finetuned-skin-cancer"
 headers = {"Authorization": f"Bearer {API_TOKEN}"}
 
-def query(payload):
-    data = json.dumps(payload)
+def query(data):
+    """Send a request to the API and return the response."""
     response = requests.request("POST", API_URL, headers=headers, data=data)
     return json.loads(response.content.decode("utf-8"))
 
-def load_image(image_file):
-    img = Image.open(image_file)
-    return img
-
-if __name__ == '__main__':
+if name == '__main__':
 
     page_title = "Skin cancer detection"
     page_icon = ":monkey:"
@@ -29,13 +25,17 @@ if __name__ == '__main__':
     image_file = st.file_uploader("Upload Images", type=["png","jpg","jpeg"])
 
     if image_file is not None:
+        st.image(image_file, caption='Input Image', use_column_width=True)
+        image_bytes = image_file.getvalue()
 
-              # To View Uploaded Image
-        st.image(load_image(image_file),width=250)
+        st.subheader("Prediction")
 
-        data = query(
-            {
-                "inputs": image_file,
-                # "parameters": {"do_sample": False},
-            }
-        )
+        while True:
+            data = query(image_bytes)
+            if "error" in data:
+                est_time = data["estimated_time"]
+                st.error(f"Model is loading. Please wait {est_time} seconds.")
+                time.sleep(4)
+            else:
+                st.write(data)
+                break
