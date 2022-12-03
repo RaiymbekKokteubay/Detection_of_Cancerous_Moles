d77e666bcf7ee29166002fcfe42329b93952c5aa
armanbolatov <a.bol@bc-pf.org>
2022-12-03 16:53:38 +0600

    bug fixes

--- main.py
+++ main.py
@@ -7,9 +7,12 @@ import time
 
 API_TOKEN = st.secrets['api_token']
 API_URL = st.secrets['api_url']
-# API_URL = "https://api-inference.huggingface.co/models/gianlab/swin-tiny-patch4-window7-224-finetuned-skin-cancer"
-# API_TOKEN = "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"
 headers = {"Authorization": f"Bearer {API_TOKEN}"}
+
+links = {
+    "Melanoma": "https://www.wikipedia.org/wiki/Melanoma",
+    "Vascular-lesions": "https://en.wikipedia.org/wiki/Vascular_anomaly",
+}
 
 def query(data):
     """Send a request to the API and return the response."""
@@ -18,13 +21,12 @@ def query(data):
 
 if __name__ == '__main__':
 
-    print('adasdasd')
     page_title = "Skin cancer detection"
     page_icon = "🐒"
     layout = "centered"
 
     st.set_page_config(page_title=page_title, page_icon=page_icon, layout=layout)
-    st.title(page_title + " " + page_icon)
+    st.title(page_title)
 
     st.write("""
         Hello! This app is an ongoing project that helps to promote cancer awareness.
@@ -42,27 +44,34 @@ if __name__ == '__main__':
     if image_file is not None:
         st.image(image_file, caption='Input Image', use_column_width=True)
         image_bytes = image_file.getvalue()
-        st.subheader("Prediction:")
+        st.subheader("Model output")
 
-        while True:
+        with st.spinner("Waiting for the prediction..."):
             data = query(image_bytes)
-            if "error" in data:
-                with st.spinner(data["error"]):
-                    time.sleep(10)
-            else:
-                scores = [e['score'] for e in data]
-                labels = [e['label'] for e in data]
-                df = pd.DataFrame({'scores': scores, 'labels': labels})
-                st.write(df)
-                fig = px.bar(df, x='scores', y='labels', orientation='h', color='scores')
-                st.write(fig)
-                st.write("""
-                    The scores above represent the probability of having a skin cancer
-                    of a particular type. The higher the score, the higher the probability.
-                    Note that the model is not perfect, and one should see a doctor for
-                    a professional observation.
+            while "error" in data:
+                time.sleep(4)
+                data = query(image_bytes)
+        st.success("Done!")
 
-                    Also, if the model returns a score of below 0.7 for all the classes, it means
-                    that the lesion is probably not a skin cancer.
-                """)
-                break
+        scores = [e['score'] for e in data]
+
+        labels = []
+        for e in data:
+            if e['label'] in links:
+                labels.append(f"<a href='{links[e['label']]}' target='_blank'>{e['label']}</a>")
+            else:
+                labels.append(e['label'])
+
+        df = pd.DataFrame({'scores': scores, 'labels': labels})
+        st.write(df.to_html(escape=False, index=False), unsafe_allow_html=True)
+        fig = px.bar(df, x='scores', y='labels', orientation='h', color='scores')
+        st.write(fig)
+        st.write("""
+            The scores above represent the probability of having a skin cancer
+            of a particular type. The higher the score, the higher the probability.
+            Note that the model is not perfect, and one should see a doctor for
+            a professional observation.
+
+            Also, if the model returns a score of below 0.7 for all the classes, it means
+            that the lesion is probably not a skin cancer.
+        """)
